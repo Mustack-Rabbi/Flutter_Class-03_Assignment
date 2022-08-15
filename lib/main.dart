c500fa3b34d3a77caabdb6054d9ca0c8db605929
mustack-rabbi <mustackrabbi@gmail.com>
2022-08-15 20:03:08 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Class-03 Assignment : Login Page"),
+      ),
+      body: const Center(child: Text("Login Page")),
+    );
+  }
+}
