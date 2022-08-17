3dc676c461da6cad0955cf725060ae1567662e03
mustack-rabbi <mustackrabbi@gmail.com>
2022-08-17 12:27:29 +0600

    Login UI

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## Flutter Class 3:
 
-> **Class Topic:** Project Name, test file, import package, void, statelessWidget, debag Banner, SafeArea, TextStyle, const, ThemeData, container, Row, Stack, SizedBox, Textfield, ListView.. etc
+> **Class 3 Topic:** Project Name, test file, import package, void, statelessWidget, debag Banner, SafeArea, TextStyle, const, ThemeData, container, Row, Stack, SizedBox, Textfield, ListView.. etc
 
 ---
 
 ## Flutter Class 3 Assignment:
 
-Login Page
+1.Create a Login UI
 
 ---
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,11 +20,77 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flutter Class-03 Assignment : Login Page"),
-      ),
-      body: const Center(child: Text("Login Page")),
-    );
+        appBar: AppBar(
+            backgroundColor: Colors.amber,
+            title: const Text(
+              "Flutter Class-03 Assignment : Create a Login UI",
+              style: TextStyle(color: Colors.white),
+            )),
+        body: Column(
+          children: [
+            const Icon(
+              Icons.account_circle,
+              color: Colors.amber,
+              size: 150.0,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text("Login UI"),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Email',
+                        icon: Icon(Icons.email, color: Colors.white),
+                        border: InputBorder.none),
+                  ),
+                ),
+              ),
+            )
+          ],
+        )
+
+        /*    ListView(
+          children: [
+            Icon(
+              Icons.account_circle,
+              color: Colors.amber,
+              size: 200.0,
+            ),
+            Center(
+              child: Container(
+                color: Colors.amber,
+                width: width,
+                height: height,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [Text("Login"), TextField()],
+                    ),
+                    Row(
+                      children: [Text("Login")],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ) */
+
+        //const Center(child: Text("Login Page")),
+        );
   }
 }
